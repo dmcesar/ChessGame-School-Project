374fbab19933ea1b0881f58156ee9021a23e7260
DiogoMourao <a21804304@alunos.ulht.pt>
2018-11-29 23:48:22 +0100

    v1.9: 2 testes + Diagrama UML

--- src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
+++ src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
@@ -43,4 +43,41 @@ public class TestSimulador {
 
         assertEquals(resultadoEsperado, resultadoReal);
     }
+
+    @Test
+    public void test02ProcessaJogada(){
+
+        int size = 4;
+
+        Simulador.tabuleiro = new CrazyPiece[size][size];
+
+        Simulador.blackTeam = new Equipa(0);
+        Simulador.whiteTeam = new Equipa(1);
+
+        CrazyPiece testPiece01 = new CrazyPiece(1, 0, 0, "testPiece01");
+        CrazyPiece testPiece02 = new CrazyPiece(2, 0, 1, "testPiece02");
+
+        blackTeam.crazyPieces.add(testPiece01);
+        blackTeam.inGameCrazyPieces.add(testPiece01);
+
+        whiteTeam.crazyPieces.add(testPiece02);
+        whiteTeam.inGameCrazyPieces.add(testPiece02);
+
+
+        //Movimento horizontal para a direita
+        int xO = 0, yO = 0;
+        int xD = 0, yD = 1;
+
+        tabuleiro[yO][xO] = testPiece01;
+        tabuleiro[yD][xD] = testPiece02;
+
+
+        //Teste: Mover a peça 'testPiece01' para as coordenadas da peça 'testPiece02'
+        boolean resultadoEsperado = true;
+
+        //Deve retornar 'true', pois existem peças em ambas as posições de equipas opostas e dentro do tabuleiro e a distância entre as duas é 1
+        boolean resultadoReal = processaJogada(xO, yO, xD, yD);
+
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
 }
